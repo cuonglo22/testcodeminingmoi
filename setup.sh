a8a1ee8c84c53cddcbd126e2796dc73ed5e6785b
nero-maple <162573302+nero-maple@users.noreply.github.com>
2024-03-20 15:31:40 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -42,5 +42,8 @@ npm install -g pm2@5.3.1
 # Khởi động ứng dụng theo tập tin cấu hình ecosystem.config.js bằng PM2
 pm2 start ecosystem.config.js
 
+# Chờ 10 giây trước khi thực thi script random.sh
+sleep 10
+
 # Thực hiện script random.sh
 bash random.sh
